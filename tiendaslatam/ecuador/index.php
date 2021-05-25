00e8a643f8f54f54ffa9c5abcc2f061435e7cf61
wilber <parionahuaraca@gmail.com>
2021-05-25 11:25:34 -0500

    cambios

--- tiendaslatam/ecuador/index.php
+++ tiendaslatam/ecuador/index.php
@@ -17,7 +17,7 @@
      <meta property="og:image" content="/tiendaslatam/files/images/banner-ecuador.jpg">
      <link rel="stylesheet" href="/assets/css/common.css">
      <link rel="stylesheet" href="/assets/css/app.css">
-     <link rel="stylesheet" href="/assets/css/tiendaslatam.css?v=1">
+     <link rel="stylesheet" href="/assets/css/tiendaslatam.css?v=2">
  </head>
 
  <body id="top" class=" ready ">
