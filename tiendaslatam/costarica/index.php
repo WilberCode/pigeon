b065fb6dc3efed5c115855facd2342f785b24277
wilber <parionahuaraca@gmail.com>
2021-05-27 17:46:41 -0500

    cambios

---
+++ tiendaslatam/costarica/index.php
@@ -0,0 +1,164 @@
+ <!DOCTYPE html>
+ <html lang="es">
+
+ <head>
+     <meta charset="utf-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <title>Costa Rica | Tiendas LATAM | Pigeon </title>
+     <meta name="description" content="Tiendas Online, Tiendas Físicas y contactos para Costa Rica">
+     <meta name="keywords" content="">
+     <meta name="viewport" content="width=device-width,initial-scale=1">
+     <meta name="format-detection" content="telephone=no">
+     <meta property="og:type" content="website">
+     <meta property="og:title" content="Costa Rica | Tiendas LATAM | Pigeon">
+     <meta property="og:site_name" content="Costa Rica">
+     <meta property="og:url" content="/tiendaslatam/costarica/">
+     <meta property="og:description" content="Tiendas Online, Tiendas Físicas y contactos para Costa Rica">
+     <meta property="og:image" content="/tiendaslatam/files/images/banner-costarica.jpg">
+     <link rel="stylesheet" href="/assets/css/common.css">
+     <link rel="stylesheet" href="/assets/css/app.css">
+     <link rel="stylesheet" href="/assets/css/tiendaslatam.css?v=2">
+ </head>
+
+ <body id="top" class=" ready ">
+     <noscript>Es posible que Javascript no se muestre correctamente porque está desactivado.</noscript>
+     <!-- [ WRAP ] -->
+     <div id="l-wrap">
+         <!-- [ GLOBAL HEADER ] -->
+         <?php include '../../includes/header.php' ?>
+         <!-- [ /GLOBAL HEADER ] -->
+
+         <!-- [ CONTENT ] -->
+         <main class="bg-white">
+             <div class="m-txt-ttl">
+                 <div data-fixed>
+                     <div class="ttl-inner">
+                         <h1 class="ttl-txt">Costa Rica </h1>
+                     </div>
+                 </div>
+             </div>
+             <div>
+                 <div class="content-inner mt-24 sm:mt-25  " data-fixed="">
+                     <ul class="tl-countries">
+                         <li><a href="/tiendaslatam/bolivia"  >Bolivia</a></li>
+                         <li><a href="/tiendaslatam/chile">Chile</a></li>
+                         <li><a href="/tiendaslatam/colombia">Colombia</a></li>
+                         <li><a href="/tiendaslatam/Costa"  class="tl-countries-active">Costa Rica</a></li>
+                         <li><a href="/tiendaslatam/guatemala">Guatemala</a></li>
+                         <li><a href="/tiendaslatam/ecuador">Ecuador</a></li>
+                         <li><a href="/tiendaslatam/mexico">México</a></li>
+                         <li><a href="/tiendaslatam/panama">Panamá</a></li>
+                         <li><a href="/tiendaslatam/peru">Perú</a></li>
+                     </ul>
+                     <div class="mt-24 sm:mt-25 mb-24 text-center">
+                         <img class=" w-46 sm:w-57 md:w-60 " src="/assets/svg/logo.svg" alt="logo pigeon latam">
+                     </div>
+                 </div>
+                 <div class="text-center max-w-tl-lg m-auto ">
+                     <img src="/tiendaslatam/files/images/banner-costarica.jpg"
+                         alt="Banner Costa Rica">
+                 </div>
+             </div>
+
+             <div class="content-inner pb-20" data-fixed="">
+                 <div class="max-w-tl-md m-auto ">
+                     <div class="tl-info  ">
+                         <div class="tl-socialmedia-wrap  ">
+                             <ul class="tl-socialmedia">
+                                 <li><a href="https://facebook.com/pigeonbol"> <img
+                                             src="/tiendaslatam/files/images/facebook.svg" alt="Facebook Costa Rica"> </a>
+                                 </li>
+                                 <li><a href="https://www.instagram.com/pigeon/costarica"><img src="/tiendaslatam/files/images/instagram.svg"
+                                             alt="Instagram Costa Rica"></a></li>
+                             </ul>
+                         </div>
+                         <div> 
+                             <table class="tl-contact  " >
+                                <tbody> 
+                                    <tr>
+                                        <th>Distribuidor</th>
+                                        <!-- <td>Raices Import Export Ltda.</td>  -->
+                                    </tr>
+                                    <tr> 
+                                        <th>Teléfono</th>
+                                       <!--  <td>(591)- 7800-8964</td>  -->
+                                    </tr>
+                                    <tr> 
+                                        <th>E-mail</th>
+                                       <!--  <td>raicespigeon@cotas.com.bo</td> -->
+                                    </tr>  
+                                </tbody> 
+                             </table>
+                         </div>
+                     </div>
+                     <div class="pt-14">
+                         <div class="tl-stories-wrap">
+                             <ul class="tl-stories">
+                                 <li><span>Tiendas Online</span></li>
+                                 <li><a href="http://bbxpress.co.cr/web/buscar?controller=search&orderby=position&orderway=desc&poscats=&search_query=pigeon&submit_search=Buscar">BBXpress</a></li>
+                                 <li><a href="https://www.bebemundo.co.cr/jolisearch?orderby=position&orderway=desc&s=pigeon&submit_search=">Bebe Mundo</a></li>
+                                 <li><a href="https://www.aliss.com/costa-rica/">Aliss</a></li>
+                                 <li><a href="https://littlenani.minidux.com/categories/20/pigeon">Little Nani</a></li>
+                                 <li><a href="https://www.motherspcr.com/ecommerce/index.php?s=pigeon&cat=-1&page=product-category">Mother’s Place</a></li>
+                             </ul>
+                         </div>
+                         <div class=" tl-stories-wrap">
+                            <!--  <ul class="tl-stories">
+                                 <li><span>Tiendas Físicas</span></li>
+                                 <li>Farmacorp</li>
+                                 <li>Farmacias Chávez</li>
+                                 <li>Farmacias Hipermaxi</li>
+                                 <li>Wawita a Bordo</li>
+                                 <li>La Casa del Bebé</li>
+                                 <li>Tienda Taiss</li>
+                                 <li>Farmacias Nueva Zelanda</li>
+                                 <li>Farmacias San Agustín</li>
+                                 <li>Farmacias Económica</li>
+                                 <li>Farmacias Copacabana</li>
+                                 <li>Farmacias Corporación Costa Ricanap</li>
+                             </ul> -->
+                         </div>
+                     </div>
+                 </div>
+
+             </div>
+
+             <div class="content-pagepath" data-fixed="">
+                 <ul class="m-box-pagepath">
+                     <li><a href="/">Home</a></li>
+                     <li><a href="/tiendaslatam/">Tiendas LATAM</a></li>
+                     <li> <span>Costa Rica</span></li>
+                 </ul>
+             </div>
+         </main>
+
+         <!-- [ /CONTENT ] -->
+
+         <!-- [ GLOBAL FOOTER ] -->
+         <?php include '../../includes/footer.php';?>
+         <!-- [ /GLOBAL FOOTER ] -->
+
+         <div class="hide-tab" data-device></div>
+         <div class="hide-pc" data-device></div>
+     </div>
+     <!-- [ WRAP ] -->
+     <script src="/assets/js/library.js"></script>
+     <script src="/assets/js/setting.js"></script>
+     <script src="/assets/js/function.js"></script>
+     <script src="/assets/js/common.js"></script>
+     <script src="/assets/js/search.js"></script>
+
+
+
+     <!--[if lte IE 9]>
+<script src="/assets/js/lib/flexibility/flexibility.js"></script>
+<script>
+$(function(){
+flexibility(document.documentElement);
+});
+</script>
+<![endif]-->
+
+ </body>
+
+ </html>
