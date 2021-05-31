47c2b884b29859f0efe30dddb875cac9f0f7273d
wilber <parionahuaraca@gmail.com>
2021-05-31 13:26:56 -0500

    cambios

--- preguntasfrecuentes/index.php
+++ preguntasfrecuentes/index.php
@@ -18,6 +18,7 @@
     <meta property="og:image" content="/assets/images/ogp.png">
     <link rel="stylesheet" href="/assets/css/common.css">
     <link rel="stylesheet" href="/assets/css/app.css"> 
+    <link rel="stylesheet" href="/assets/css/frequentquestions.css"> 
 
     <script>
     
@@ -48,9 +49,190 @@
 				</div>
 			</div>  
             <div class="content-inner" data-fixed="">
-                <img src="/lineahospitalaria/files/images/LineaHospitalariaMuyPronto.jpg" alt="Linea Hospitalaria Muy Pronto">
+                <div class="mt-24 sm:mt-25 mb-24 text-center">
+                    <img class=" w-46 sm:w-57 md:w-60 " src="/assets/svg/logo.svg" alt="">
+                </div>
+                <img src="/preguntasfrecuentes/files/images/pigeon_preguntas_frecuentes.jpg" alt="Preguntas Frecuentes - Pigeon">
             </div>
+            <div>
+                 <div class="content-inner mt-[2.6rem] " data-fixed="">
+                    <ul class="faqs-topics" id="faqs-topics" >
+                        <li><a href="#topic1">Medio Ambiente</a></li>
+                        <li><a href="#topic2">Lactancia</a></li>
+                        <li><a href="#topic3">Salud del Bebé</a></li>
+                        <li><a href="#topic4">Alimentación o Destete</a></li>
+                        <li><a href="#topic5">Tiendas</a></li>
+                        <li><a href="#topic6">Varios</a></li> 
+                    </ul> 
+                </div>
+            </div>
+            <div class="content-inner" data-fixed=""> 
+                <div class=" mb-30 mt-[5rem]  sm:mt-[7rem] max-w-[1048px] mx-auto " >
+                    <div class="faqs-topic" id="topic1" >
+                        <h2  class="faqs-title" >Medio Ambiente</h2>
+                       <div class="faqs-body">
+                            <div class="faqs-body-item" >
+                                <h3 > <span> P: </span> ¿Qué acciones toma la marca Pigeon con respecto al impacto ambiental que tiene
+                                su tipo de negocio, hacia con el ambiente?</h3>
+                                <p><span>R:</span> Por el momento no contamos con un plan eco-amigable puesto en marcha al 100%, pero aseguramos que la
+                                marca se encuentra en constantes cambios, y parte de estos cambios está en la transición de empaques plásticos a
+                                cartón. Pigeon es una marca cuya misión es crear un entorno más baby-friendly, y parte de ello es el de crear un
+                                mundo más verde que permitirá el continuo crecimiento saludable de sus habitantes. 
+                                </p>
+                               
+                            </div>
+                            <div class="faqs-body-line" ></div>
+                            <div class="faqs-body-item" >
+                                <h3 > <span> P: </span>¿Qué es el BPA? </h3>
+                                                                    <p><span>R:</span> Es un químico que ha sido utilizado desde los años 1960s, este químico tiene su uso común en materiales de
+                                    resina y plástico.
+                                </p> 
+                                <img src="/preguntasfrecuentes/files/images/Que_es_el_BPA.jpg" alt="¿Qué acciones toma la marca Pigeon con respecto al impacto ambiental que tiene
+                                su tipo de negocio, hacia con el ambiente?"> 
+                            </div>
+                       </div>
+                    </div>
+                    <div class="faqs-topic" id="topic2">
+                        <h2  class="faqs-title" >Lactancia</h2>
+                       <div class="faqs-body">
+                            <div class="faqs-body-item" >
+                                <h3 > <span> P: </span>  ¿Cómo se puede prevenir la Mastitis?</h3>
+                                <p><span>R:</span> Entre los mejores métodos se encuentra: 1) Que el bebé vacíe por completo uno de los senos, esto antes de
+                                cambiar al otro seno durante la lactancia. 2) Es recomendable variar la posición que utiliza al dar de lactar entre una
+                                alimentación y otra. 
+                                </p> 
+                                <img src="/preguntasfrecuentes/files/images/Como_se_puede_prevenir_la_Mastitis.jpg" alt="¿Cómo se puede prevenir la Mastitis?"> 
+                            </div>
+                            <div class="faqs-body-line" ></div>
+                            <div class="faqs-body-item" >
+                                <h3 > <span> P: </span> ¿Qué biberón es más recomendable para un bebé menor de 6 meses?</h3>
+                                <p><span>R:</span> Todo depende de cada mamá y que tan cómoda se encuentre ella con el biberón de un material específico. Pero
+                                cabe destacar que un biberón de vidrio permite que se mantenga mejor la lecha materna o formula dentro de ella. La
+                                misma es más fácil de limpiar con profundidad y es resistente a altas temperaturas.
+                                </p> 
+                                <img src="/preguntasfrecuentes/files/images/3_biberones.jpg" alt=" ¿Qué biberón es más recomendable para un bebé menor de 6 meses?"> 
 
+                            </div>
+                            <div class="faqs-body-line" ></div>
+                            <div class="faqs-body-item" >
+                                <h3 > <span> P: </span> ¿Qué biberón es más recomendable para un bebé mayor a 6 meses?</h3>
+                                <p><span>R:</span>Todo depende de cada mamá y que tan cómoda se encuentre ella con el biberón de un material específico.
+                                Pero cabe destacar que un biberón de polipropileno permite que el bebé pueda agarrar el biberón de manera segura
+                                durante el tiempo que esté tomando leche por medio de este producto. Los biberones de este material son
+                                    resistentes a altas temperaturas, son seguros para los bebés y son ligeros al momento de su uso.
+
+                                </p> 
+                                <img src="/preguntasfrecuentes/files/images/biberones_para_mayor_de_6meses.jpg" alt="¿Qué biberón es más recomendable para un bebé mayor a 6 meses?">  
+                            </div>
+                            <div class="faqs-body-line" ></div>
+                            <div class="faqs-body-item" >
+                                <h3 > <span> P: </span> Diferencias entre Biberones Flexible y Biberones SofTouch</h3>
+                                <p><span>R:</span> FLEXIBLE: Se adapta a la boca del bebé / Super estirable / Máxima higiene / Sistema anticólicos / Interior ranurado
+                                    para máxima flexibilidad. <br class="hide-sp" > 
+                                    SOFTOUCH: Desarrollado para que el bebé mantenga el mismo estilo de succión natural empleado durante la
+                                    lactancia / Sistema anticólicos / Encaja perfecta en la fosa de succión de la boca del bebé.
+
+                                </p> 
+                                <img src="/preguntasfrecuentes/files/images/Biberones_SofTouch.jpg" alt="Diferencias entre Biberones Flexible y Biberones SofTouch">  
+                            </div>
+                            <div class="faqs-body-line" ></div>
+                            <div class="faqs-body-item" >
+                                <h3 > <span> P: </span>  Uso correcto y recomendado, para el Extractor de Pera o Saca Leche</h3>
+                                <p><span>R:</span> Este producto apoya durante el proceso de lactancia. El producto fue creado con el propósito de apoyar a las
+                                        mamás una vez hayan realizado todo el proceso de lactancia, y estas quieran asegurarse de evitar una mastitis o el
+                                        de mantener residuos de leche dentro del pecho. El producto es de uso fácil y de material de caucho. No es
+                                        recomendable que la leche materna que toque este Extractor de Pera o Saca Leche sea dado como alimento al bebé. 
+
+                                </p> 
+                                <img src="/preguntasfrecuentes/files/images/Saca_Leche.jpg" alt=" Uso correcto y recomendado, para el Extractor de Pera o Saca Leche">  
+                            </div>
+                            <div class="faqs-body-line" ></div>
+                            <div class="faqs-body-item" >
+                                <h3 > <span> P: </span>Uso correcto y recomendado, para el Recolector de Leche Materna o Milk Saver Pump</h3>
+                                <p><span>R:</span> Este producto es un apoyo durante el proceso de lactancia. Fue creado con el propósito de apoyar a las mamás
+                                    una vez hayan realizado todo el proceso de lactancia, y estas quieran asegurarse de evitar perdidas de leche
+                                    materna luego de la lactancia; o tambien durante el periodo en que la madre se encuentra lactando al bebé.
+                                    Este producto permite que las mamás recolecten y almacenen la leche materna para su uso posteriormente.
+                                    El Recolector de Leche Materna fue creado con un diseño ergonómico y ligero. 
+                                </p> 
+                                <img src="/preguntasfrecuentes/files/images/Milk_Saver_Pump.jpg" alt=" Uso correcto y recomendado, para el Recolector de Leche Materna o Milk Saver Pump">  
+                            </div>
+                            <div class="faqs-body-line" ></div>
+                            <div class="faqs-body-item" >
+                                <h3 > <span> P: </span>¿Cómo debo de almacenar la leche materna?</h3>
+                                <p><span>R:</span> : La leche materna se expande al congelarse. Evitar llenar las botellas / bolsas más de tres cuartos de su capacidad.
+                                            Refrigera o congela la leche lo antes posible tras su extracción. <br >
+                                            Luego de la extracción etiqueta las botellas / bolsas con la fecha, así podrás esocger la más antigua primero. <br >
+                                            Duración hasta 4 horas a temperatura ambiente (16 - 25 Cº) <br >
+                                            Duración hasta 3 días en el refrigerador (4 Cº o menos) <br >
+                                            Duración hasta 9 meses en el congelador (-18 Cº o menos) 
+                                </p> 
+                                <img src="/preguntasfrecuentes/files/images/almacenar_la_leche_materna.jpg" alt=" ¿Cómo debo de almacenar la leche materna?">  
+                            </div>
+                       </div>
+                    </div>
+                    <div class="faqs-topic" id="topic3">
+                        <h2  class="faqs-title" >Salud del Bebé</h2>
+                       <div class="faqs-body">
+                            <div class="faqs-body-item" >
+                                <h3 > <span> P: </span> ¿Cuándo es más preciso una lectura de termómetro digital?</h3>
+                                <p><span>R:</span>  La edad más recomendada para obtener una lectura, por termómetro digital, es a los seis (6) meses del bebé.
+                                    Sobre todo cuando se toma la temperatura por medio rectal.
+                                </p>
+                                <img src="/preguntasfrecuentes/files/images/termometro_digital.jpg" alt="¿Cuándo es más preciso una lectura de termómetro digital?"> 
+
+                            </div>
+                            <div class="faqs-body-line" ></div>
+                            <div class="faqs-body-item" >
+                                <h3 > <span> P: </span>¿Cómo saber que cepillo dental es el ideal para mi bebé?</h3>
+                                <p><span>R:</span>  
+                                    La elección del cepillo dental para tu bebé debe ir de la mano de dos factores: 1) los meses de tu bebé, y 2)
+                                    lo aconsejado por el odontólogo que atiende a tu bebé. <br>
+                                    PASO 1: 6 a +8 meses <br>
+                                    PASO 1: 8 a +12 meses <br>
+                                    PASO 3: 12 a +18 meses <br>
+                                </p> 
+                                <img src="/preguntasfrecuentes/files/images/cepillo_dental.jpg" alt="¿Cómo saber que cepillo dental es el ideal para mi bebé?"> 
+                            </div>
+                            <div class="faqs-body-line" ></div>
+                            <div class="faqs-body-item" >
+                                <h3 > <span> P: </span>¿Qué función tiene este Aspirador Nasal?</h3>
+                                <p><span>R:</span>  
+                                    Facilita la respiración del bebé al limpiar las vías respiratorias. <br>
+                                    Disminuye el riesgo de infecciones al eliminar el flujo nasal. <br>
+                                    Alivia las molestias de tu bebé.
+                                </p> 
+                                <img src="/preguntasfrecuentes/files/images/Aspirador_Nasal.jpg" alt="¿Qué función tiene este Aspirador Nasal?"> 
+                            </div>
+                       </div>
+                    </div>
+                    <div class="faqs-topic" id="topic4">
+                        <h2  class="faqs-title" >Alimentación o Destete</h2>
+                       <div class="faqs-body">
+                            <div class="faqs-body-item" >
+                                <h3 > <span> P: </span>  ¿Cuándo es bueno que mi bebé comience a comer comida semisólida?</h3>
+                                <p><span>R:</span>  Desde los seis (6) meses es recomendable que el bebé inicie su transición de biberón a comida semisólida. Esto le
+                                permitirá reconocer sabores y texturas que también aportarán a su desarrollo cognitivo.
+                                </p>
+                                <img src="/preguntasfrecuentes/files/images/comience_a_comer_comida.jpg" alt=" ¿Cuándo es bueno que mi bebé comience a comer comida semisólida?"> 
+
+                            </div>  
+                       </div>
+                    </div>
+                    <div class="faqs-topic" id="topic5">
+                        <h2  class="faqs-title" >Tiendas LATAM</h2>
+                       <div class="faqs-body">
+                            <div class="faqs-body-item" >
+                                <h3 > <span> P: </span>¿Dónde puedo encontrar productos Pigeon?</h3>
+                                <p><span>R:</span>  Estamos presentes en los siguientes países de Latinoamérica: Bolivia / Chile / Colombia / Costa Rica / Guatemala /
+                                            Ecuador / México / Panamá / Perú. <a  href="/tiendaslatam/">Ingresa AQUÍ</a> para ver las tiendas disponibles en tu país.
+                                </p> 
+                            </div>  
+                       </div>
+                    </div>
+                </div>
+                <div  class="h-20" ></div>
+           </div>   
             <div class="content-pagepath" data-fixed="">
                 <ul class="m-box-pagepath">
                     <li><a href="/">Home</a></li>
@@ -74,8 +256,7 @@
     <script src="/assets/js/function.js"></script>
     <script src="/assets/js/common.js"></script>
     <script src="/assets/js/search.js"></script> 
-
- 
+  
 
     <!--[if lte IE 9]>
 <script src="/assets/js/lib/flexibility/flexibility.js"></script>
