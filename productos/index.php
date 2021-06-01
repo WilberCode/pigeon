bc9ddad6401ff7c3a405eeb8b8691f3149b8018d
wilber <parionahuaraca@gmail.com>
2021-06-01 10:31:23 -0500

    cambios

--- productos/index.php
+++ productos/index.php
@@ -24,7 +24,7 @@
     <meta property="og:image" content="/productos/files/images/Limpieza_y_Esterilizacion.jpg">
     <link rel="stylesheet" href="/assets/css/common.css">
     <link rel="stylesheet" href="/assets/css/app.css">
-    <link rel="stylesheet" href="/assets/css/products.css">
+    <link rel="stylesheet" href="/assets/css/products.css?v=1">
 </head>
 
 <body id="top">
