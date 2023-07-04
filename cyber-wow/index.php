23f2cea7c088692997eed6b668d6c7628f3792ab
wilber <parionahuaraca@gmail.com>
2023-07-03 20:58:14 -0500

    cambiuos

---
+++ cyber-wow/index.php
@@ -0,0 +1,73 @@
+ <?php include '../includes/config.php' ?>  
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cyber WOW - Pigeon</title>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="canonical" href="<?=theCurrentUrl();?>" />
+    <meta name="keywords" content="Cyber WOW">
+    <meta name="description" content="Descuento de productos hasta 30% off, del 3 al 7 de Julio.">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="Cyber WOW - Pigeon">
+    <meta property="og:site_name" content="Cyber WOW - Pigeon">
+    <meta property="og:url" content="<?=theCurrentUrl();?>"/>
+    <meta property="og:description" content="Descuento de productos hasta 30% off, del 3 al 7 de Julio.">
+    <meta property="og:image" content="images/PigeonCWlanding-cuerpo.jpg?v=<?=theVersion();?>">  
+    <style>
+        html{box-sizing:border-box;margin:0;padding:0}*,:after,:before{box-sizing:inherit;margin:inherit;padding:inherit}img{display:block}.flex{display:flex}.justify-center{justify-content:center}.items-start{align-items:center}.w-full{width:100%}.max-w-288{max-width:288px}.max-w-108{max-width:108px}.max-w-254{max-width:254px}.max-w-650{max-width:650px}
+        .cyber-wow-link{
+            font-size: 0;
+            display: block;
+            margin:0;
+        }
+        .pt-2{
+            margin-top: 2px;
+        }
+        img{
+            margin:0;
+        }
+   </style>
+</head>
+
+<body >    
+      <div  class="flex justify-center items-start  "> 
+         <main > 
+            <div class="flex justify-center  items-center w-full pt-2" >
+               <div>
+                  <a class="cyber-wow-link"  href="https://www.falabella.com.pe/falabella-pe/search?Ntt=pigeon+extractor">
+                     <img class="  w-full max-w-288" src="images/PigeonCWlanding-falabella.jpg" alt="Falabella">
+                     Falabella
+                  </a>
+               </div>
+               <div>
+                  <a class="cyber-wow-link"  href="https://simple.ripley.com.pe/search/pigeon?sort=score&page=1">
+                     <img class=" w-full max-w-108" src="images/PigeonCWlanding-ripley.jpg" alt="Ripley">
+                     Ripley
+                  </a>
+               </div>
+               <div>
+                  <a class="cyber-wow-link"  href="https://linio.falabella.com.pe/linio-pe/search?Ntt=pigeon "> 
+                     <img class=" w-full max-w-254" src="images/PigeonCWlanding-lineo.jpg" alt="lineo">
+                     Lineo
+                  </a>
+
+               </div>
+            </div> 
+            <div>
+             <img  class=" w-full max-w-[650px]"  src="images/PigeonCWlanding-cuerpo.jpg" alt="cyber promo">
+            </div>
+
+         </main >
+
+      </div>     
+	<script src="/assets/js/search.js?v=685656"></script>
+ 
+
+</body>
+
+</html>
