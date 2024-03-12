dfa6559240f7c1a5cfc22b1d0c16d8246a0e3b44
wilber <parionahuaraca@gmail.com>
2024-03-11 21:17:20 -0500

    cambios

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,41 @@
+<script src="https://www.google.com/recaptcha/api.js" async defer></script>
+
+<?php 
+
+$dotenv = parse_ini_file('.env');
+
+$clave_sitio = $dotenv['TU_CLAVE_DEL_SITIO'];
+$recaptcha_secret = $dotenv['TU_CLAVE_SECRETA'];
+ 
+$recaptcha_response = $_POST['g-recaptcha-response'];
+
+$url = 'https://www.google.com/recaptcha/api/siteverify';
+$data = [
+    'secret' => $recaptcha_secret,
+    'response' => $recaptcha_response
+];
+$options = [
+    'http' => [
+        'header' => "Content-type: application/x-www-form-urlencoded\r\n",
+        'method' => 'POST',
+        'content' => http_build_query($data)
+    ]
+];
+$context = stream_context_create($options);
+$verify = file_get_contents($url, false, $context);
+$captcha_success = json_decode($verify);
+$message_captcha = null;
+$captcha = false;
+if ($captcha_success->success == false) {
+    $message_captcha =  "<p>Validación incorrecta</p>"; 
+} else if ($captcha_success->success == true) {
+    $message_captcha = "<p>Validación correcta</p>";
+	$captcha = true;
+}
+
+
+
+?>
 <div class="modal-wrap" id="modal-wrap"  >
 		<div  class="modal-close-wrap" id="modal-close-wrap" ></div>
 		<div  class="modal" > 
@@ -46,7 +84,11 @@
 								</div>
 								
 						<section>
-							<input type="submit" id="modal-send" value="QUIERO RECIBIR BENEFICIOS" class="submit boton_rojo"  >
+							<div style="position: absolute;left: 25px;bottom: -113px;">
+								<div class="g-recaptcha" data-sitekey="<?=$clave_sitio?>"></div>
+								<?=$message_captcha;?>
+							</div>
+							<input  <?=$captcha?'':'disabled'; ?> type="submit" id="modal-send" value="QUIERO RECIBIR BENEFICIOS" class="submit boton_rojo"  >
 						</section>
  					</form>
 					 
