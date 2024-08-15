43cf6d85d51aa8192208b76e966ab0575ef65788
wilber <parionahuaraca@gmail.com>
2024-08-15 15:35:23 -0500

    cambios

---
+++ pigeon-report/index.php
@@ -0,0 +1,101 @@
+ <?php include '../includes/config.php' ?> 
+ 
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Reportes Pigeon | Pigeon </title>
+    <meta name="description" content="Reportes Pigeon">
+    <meta name="keywords" content="Tiendas LATAM, Bolivia, Chile, Colombia, Costa Rica, Guatemala, Ecuador, México, Panamá, Perú , Lumilu peru, pedidos Ya, Bati Kids, Plazavea">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta name="format-detection" content="telephone=no">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="Reportes Pigeon | Pigeon">
+    <meta property="og:site_name" content="Reportes Pigeon ">
+    <meta property="og:url" content="<?=theCurrentUrl();?>">
+    <meta property="og:description" content="Reportes Pigeon">
+    <meta property="og:image" content="/tiendaslatam/files/images/tiendas-latam-banner.jpg">
+    <link rel="stylesheet" href="/assets/css/common.css">
+    <link rel="stylesheet" href="/assets/css/app.css?v=<?=theVersion();?>"> 
+    <link rel="stylesheet" href="/assets/css/tiendaslatam.css?v=<?=theVersion();?>">
+    <link rel="canonical" href="<?=theCurrentUrl();?>" />
+</head>
+
+<body id="top" class=" ready ">
+    <noscript>Es posible que Javascript no se muestre correctamente porque está desactivado.</noscript>
+    <!-- [ WRAP ] -->
+    <div id="l-wrap">
+        <!-- [ GLOBAL HEADER ] -->
+        <?php include '../includes/header.php' ?>
+        <!-- [ /GLOBAL HEADER ] -->
+
+       	<!-- [ CONTENT ] -->
+           <main   class="bg-white" >
+			<div class="m-txt-ttl"> 
+				<div data-fixed> 
+					<div class="ttl-inner">
+						 <div class="max-w-lg  ">
+							<h1 class="ttl-txt">Reportes Pigeon </h1>
+						</div>
+					</div>
+				</div>
+			</div>  
+           <div>
+               <div class="content-inner mt-24 sm:mt-25 " data-fixed=""> 
+                    <div class="mt-24 sm:mt-25 mb-24 text-center">
+                        <img class=" w-46 sm:w-57 md:w-60 " src="/assets/svg/logo.svg" alt="">
+                    </div>
+                    <ul class="tl-countries" >
+                        <li><a href="/tiendaslatam/bolivia/">Bolivia</a></li>
+                        <li><a href="/tiendaslatam/chile/">Chile</a></li>
+                        <li><a href="/tiendaslatam/colombia/">Colombia</a></li>
+                        <li><a href="/tiendaslatam/costarica/">Costa Rica</a></li>
+                        <li><a href="/tiendaslatam/guatemala/">Guatemala</a></li>
+                        <li><a href="/tiendaslatam/ecuador/">Ecuador</a></li>
+                        <li><a href="/tiendaslatam/mexico/">México</a></li>
+                        <li><a href="/tiendaslatam/panama/">Panamá</a></li>
+                        <li><a href="/tiendaslatam/peru/">Perú</a></li>
+                    </ul>
+                </div> 
+           </div>  
+
+            <div class="content-pagepath" data-fixed="">
+                <ul class="m-box-pagepath">
+                    <li><a href="/">Home</a></li>
+                    <li> <span>Tiendas LATAM</span></li> 
+                </ul>
+            </div>
+		</main>
+	  
+		<!-- [ /CONTENT ] -->
+
+        <!-- [ GLOBAL FOOTER ] -->
+        <?php include '../includes/footer.php';?>
+        <!-- [ /GLOBAL FOOTER ] -->
+
+        <div class="hide-tab" data-device></div>
+        <div class="hide-pc" data-device></div>
+    </div>
+    <!-- [ WRAP ] -->
+    <script src="/assets/js/library.js"></script>
+    <script src="/assets/js/setting.js"></script>
+    <script src="/assets/js/function.js"></script>
+    <script src="/assets/js/common.js"></script> 
+	<script src="/assets/js/search.js?v=<?=theVersion();?>"></script>
+
+ 
+
+    <!--[if lte IE 9]>
+<script src="/assets/js/lib/flexibility/flexibility.js"></script>
+<script>
+$(function(){
+flexibility(document.documentElement);
+});
+</script>
+<![endif]-->
+
+</body>
+
+</html>
